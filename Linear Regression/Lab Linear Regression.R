645a6fa1143b489ccbfff3e5b8ffc12e869aae8e
Phuoc Ho <phuochoquang97@gmail.com>
2023-03-10 11:31:56 +0700

    update lab linear regression

--- Linear Regression/Lab Linear Regression.R
+++ Linear Regression/Lab Linear Regression.R
@@ -25,4 +25,51 @@ summary(lm.fit)
 # get other pieces of information are stored in lm.fit
 names(lm.fit)
 coef(lm.fit)
+
+# obtain a confidence interval for the coefficient estimates
 confint(lm.fit)
+
+# produce confidence intervals
+predict(lm.fit, data.frame(lstat=(c(5, 10, 15))), interval="confidence")
+
+#produce prediction intervals
+predict(lm.fit, data.frame(lstat=(c(5, 10, 15))), interval="prediction")
+
+plot(lstat, medv)
+plot(lstat, medv, col="green", pch="+")
+abline(lm.fit, lwd=3, col="red")
+
+par(mfrow=c(2, 2))
+plot(lm.fit)
+plot(predict(lm.fit), residuals(lm.fit))
+plot(predict(lm.fit), rstudent(lm.fit))
+plot(hatvalues(lm.fit))
+which.max(hatvalues(lm.fit))
+
+# -------------------------------
+# MULTIPLE LINEAR REGRESSION
+# all variables
+lm.fit <- lm(medv ~ ., data = Boston)
+summary(lm.fit)
+
+# all variables except age
+lm.fit1 <- lm(medv ~ . - age, data = Boston)
+summary(lm.fit1)
+
+# interaction terms
+# lstat:black -> interaction term between lstat and black
+# medv = lstat + age + lstat*age
+lm.fit2 <- lm(medv ~ lstat * age, data = Boston)
+summary(lm.fit2)
+# -------------------------------
+
+# NON-LINEAR TRANSFORMATIONS OF PREDICTORS
+# I(X^2) = X^2
+lmfit3 <- lm(medv ~ lstat + I(lstat^2))
+summary(lmfit3)
+
+# compare 2 models
+anova(lm.fit, lmfit3)
+
+par(mfrow = c(2, 2))
+plot(lmfit3)
