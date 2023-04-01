5c389ab1865a0c82b23c81e49110a88ca15d4569
Phuoc Ho <phuochoquang97@gmail.com>
2023-04-01 12:00:05 +0700

    Applied Classification - Problem 13

---
+++ Classification/Applied Classification.R
@@ -0,0 +1,85 @@
+# QUESTION 13
+library(ISLR2)
+summary(Weekly)
+
+# scatterplot matrix
+pairs(Weekly[, 1:8])
+
+# correlation matrix
+cor(Weekly[, 1:8])
+
+# => as can be seen on the scatterplot and correlation matrix,
+# there appears to be a positive correlation between Year and Volume
+# from the summary statistic, Lag variables are very similar to each other and Today
+# there doesn't appear to be any patterns except for an increase in volume from 1989 to 2001
+
+
+attach(Weekly)
+
+# Logistic regression model
+lg_model <- glm(Direction ~ Lag1 + Lag2 + Lag3 + Lag4 + Lag5 + Volume, data = Weekly, family = binomial)
+summary(lg_model)
+# => Lag2 is statistically significant
+
+lg_probs = predict(lg_model, type = "response")
+lg_preds = rep("Down", 1089)
+lg_preds[lg_probs > 0.5] <- "Up"
+table(lg_preds, Direction)
+# => The fraction of days where predictions are correct is (54+557)/1089 ~ 56%
+# therefore, training error rate = 44%
+# of (430+557) Up predictions the model makes, it is correct 557/(430+557)% of the time
+# Actually, there are (48+557)/1089 Up, the model's accuracy when predicting Up is only slightly better than random guessing
+
+# model with only Lag2, training data period from 1990 to 2008
+train = (Year < 2009)
+test_data = Weekly[!train, ]
+test_direction = Direction[!train]
+lg_model2 = glm(Direction ~ Lag2, data = Weekly, family = binomial, subset = train)
+summary(lg_model2)
+
+# prediction on the test set
+lg_probs2 = predict(lg_model2, test_data, type = "response")
+lg_preds2 = rep("Down", 104)
+lg_preds2[lg_probs2 > .5] <- "Up"
+table(lg_preds2, test_direction)
+# => accuracy = (64) / (104) = 62.5%
+
+# ------------------------------------------------------------
+#                             LDA
+# ------------------------------------------------------------
+library(MASS)
+lda_model <- lda(Direction ~ Lag2, data = Weekly, subset = train)
+plot(lda_model)
+# prediction on the test set
+lda_pred = predict(lda_model, test_data)
+lda_class = lda_pred$class
+table(lda_class, test_direction)
+# => accuracy = (9 + 56) / 106 = 62.5%
+
+# ------------------------------------------------------------
+#                             QDA
+# ------------------------------------------------------------
+qda_model <- qda(Direction ~ Lag2, data = Weekly, subset = train)
+qda_pred = predict(qda_model, test_data)
+qda_class = qda_pred$class
+table(qda_class, test_direction)
+# => accuracy = 61 / 104 = 58%, no better than guessing each day is Up
+
+# ------------------------------------------------------------
+#                             KNN
+# ------------------------------------------------------------
+library(class)
+set.seed(1)
+train_x = Weekly[train, 3] # Lag2 column
+test_x = Weekly[!train, 3]
+train_direction = Direction[train]
+
+# changing from vector to matrix by adding dimentions
+dim(train_x) = c(985, 1)
+dim(test_x) = c(104, 1)
+
+# predictions for K = 1
+knn_pred = knn(train_x, test_x, train_direction, k = 1)
+
+table(knn_pred, test_direction)
+# => accuracy = (21 + 31) / 104 = 50%
